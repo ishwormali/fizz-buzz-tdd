9de5a08ac3938f002edbe3c679154b6373e52fe7
Ishwor Mali <ishwor.mali@terem.com.au>
2021-05-20 20:58:59 +1000

    initial test and fizzbuzz solution

---
+++ src/fiz-buzz-utils.js
@@ -0,0 +1,20 @@
+const findFizzBuzz = () => {
+  let result = [];
+  for (let i = 1; i <= 100; i++) {
+    const isFizz = i % 3 === 0;
+    const isBuzz = i % 5 === 0;
+    if (isFizz && isBuzz) {
+      result.push("FizzBuzz");
+    } else if (isFizz) {
+      result.push("Fizz");
+    } else if (isBuzz) {
+      result.push("Buzz");
+    } else {
+      result.push(i);
+    }
+  }
+
+  return result.join(",");
+};
+
+module.exports = findFizzBuzz;
