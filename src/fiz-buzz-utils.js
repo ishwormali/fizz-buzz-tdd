cfd1ea61baade27350ca907c1f28cd46edc89a0c
Ishwor Mali <ishwor.mali@terem.com.au>
2021-05-21 10:43:43 +1000

    Refactoring with constants

--- src/fiz-buzz-utils.js
+++ src/fiz-buzz-utils.js
@@ -2,17 +2,19 @@
  * Finds fizz buzz strings.
  * @returns FIzz buzz string
  */
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
 const findFizzBuzz = () => {
   let result = [];
   for (let i = 1; i <= 100; i++) {
     const isFizz = i % 3 === 0;
     const isBuzz = i % 5 === 0;
     if (isFizz && isBuzz) {
-      result.push("FizzBuzz");
+      result.push(`${FIZZ}${BUZZ}`);
     } else if (isFizz) {
-      result.push("Fizz");
+      result.push(FIZZ);
     } else if (isBuzz) {
-      result.push("Buzz");
+      result.push(BUZZ);
     } else {
       result.push(i);
     }
